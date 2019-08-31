9b42c166a35b050841580757d8005b88cf9c7526
Saniya Nafees <39744521+saniyanafees6@users.noreply.github.com>
2019-08-30 20:16:30 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
 $( document ).ready(function() {
-    // let url="none.jpg";
     // initial state
-    // $("#uploadedImage").hide()
-    // $("#predictCTA").hide()
-    // $("#h2").hide()
-    // $("#imagePreview").hide()
     $('.image-section').hide();
     $('#btn-upload').hide();
     $('#prediction').hide();
     $('#btn-predict').hide();
     $('.loader').hide();
-     // Upload Preview
-
+     
+    // Upload Preview
      function preview_image(event) 
      {
       var reader = new FileReader();
@@ -20,32 +15,22 @@ $( document ).ready(function() {
        var output = document.getElementById('previewImage');
        output.src = reader.result;
        console.log(render.result)
-    //    url=
+
       }
       reader.readAsDataURL(event.target.files[0]);
      }
-    //  function readURL(input) {
-    //     if (input.files && input.files[0]) {
-    //         var reader = new FileReader();
-    //         reader.onload = function (e) {
-    //             $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-    //             url = e.target.result;
-    //             $('#imagePreview').show();
-    //         }
-    //         reader.readAsDataURL(input.files[0]);
-    //     }
-    // }
+
+    // after user chooses an image give them an option to upload it to the app
     $("#imageUpload").change(function () {
         $('#btn-upload').show();
         $('.image-section').hide();
         $('#prediction').hide();
         $('#btn-predict').hide();
         $('.loader').hide();
-        // readURL(this);
     });
 
-    $("#btn-upload").click(function (){
-        
+    // show the image once they choose to upload it   
+    $("#btn-upload").click(function (){ 
         $('.image-section').show();
         $('#btn-predict').show();
         $('.loader').hide();
@@ -54,6 +39,7 @@ $( document ).ready(function() {
     const img = document.getElementById('previewImage');
 
   // Load the model.
+  //   perform prediction
   $('#btn-predict').click(function(){
     $('.loader').show();
     cocoSsd.load().then(model => {
